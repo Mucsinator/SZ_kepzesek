0f6bd6d0c5da1503ca2948fdf82d57ed1bf7d1c0
Mucsinator <vidazsombor66@gmail.com>
2023-12-04 14:31:08 +0100

    Sz_kepzesek

---
+++ szf_kepzesek/routes/trainings.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const {
+    getTraining,
+    getTrainings,
+    createTraining,
+    updateTraining,
+    deleteTraining,
+} = require("../controllers/trainings.js");
+const router = express.Router();
+router.route("/").get(getTrainings).post(createTraining);
+router
+    .route("/:id")
+    .get(getTraining)
+    .put(updateTraining)
+    .delete(deleteTraining);
+
+
+module.exports = router;
+
+
